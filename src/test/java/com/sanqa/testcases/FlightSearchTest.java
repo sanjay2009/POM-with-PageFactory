c59171cd5b439125f12d6d20c6e10b1c2e33e4a5
sanjay ramya <ramesh.sanjay@gmail.com>
2020-02-03 22:03:23 +0530

    POM with Page Factory fifth

--- src/test/java/com/sanqa/testcases/FlightSearchTest.java
+++ src/test/java/com/sanqa/testcases/FlightSearchTest.java
@@ -1,7 +1,7 @@
 package com.sanqa.testcases;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -16,32 +16,36 @@ public class FlightSearchTest {
 	{
 		Page.initConfiguration();
 	}
-	
-	@Test
+		
+	@Test()
 	public void flightSearchTest() 
 	{
 		
+		/*if(data.get("runmode").equalsIgnoreCase("N"))
+		{
+			throw new SkipException("Skipping since runmode is No");
+		}*/
 		//System.setProperty("webdriver.chrome.driver", "F:\\PageObjectWithPageFactory\\src\\test\\resources\\driver\\chromedriver.exe");
 		//WebDriver driver= new ChromeDriver();
 		//driver.get("https://www.expedia.co.in");
 		//HomePage home= PageFactory.initElements(driver, HomePage.class);
 		//driver.close();
-		//Page.initConfiguration();
+		
 		HomePage home = new HomePage();
 		//Assert.assertEquals(home.findTabCount(), 5);
 		Errorcollector.verifyEquals(home.findTabCount(), 5);
-		home.goToFlights().bookAFlight("Bengaluru, India (BLR-Kempegowda Intl.)", "Goa, India (GOI-Dabolim)", "06/02/2020", "09/02/2020");;
+		home.goToFlights().bookAFlight("Bengaluru, India (BLR-Kempegowda Intl.)", "Goa, India (GOI-Dabolim)", "16/02/2020", "19/02/2020");
 		//home.bookAFlight("Bangalore", "Goa", "06/02/2020", "09/02/2020");
 		
-				
-		
-		
+						
 	}
 	
-	@AfterTest
+	@AfterMethod
 	public void tesrdown()
 	{
+		if(Page.driver != null) {
 		Page.quitBrowser();
+		}
 	}
 
 }
--- src/test/java/com/sanqa/testcases/SignInTest.java
+++ src/test/java/com/sanqa/testcases/SignInTest.java
@@ -1,26 +1,44 @@
 package com.sanqa.testcases;
 
+import java.util.Hashtable;
+
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.sanqa.base.Page;
 import com.sanqa.pages.actions.SignInPage;
+import com.sanqa.utilities.Utilities;
 
 public class SignInTest {
-
-	@Test
-	public  void signInTest() {
 	
+	
+	@Test()
+	public  void signInTest() {
+		
+		/*if(data.get("runmode").equalsIgnoreCase("N"))
+		{
+			throw new SkipException("Skipping the test as runmode is NO");
+		}*/
+		
 		Page.initConfiguration();
-		Page.topNav.gotoSignIn();
+		//Page.topNav.gotoSignIn();
 		
 		//SignInPage signin = Page.topNav.gotoSignIn();
 		
-		SignInPage signin = new SignInPage();
+		SignInPage signin = Page.topNav.gotoSignIn();
 		
 		signin.doLogin("ramesh.sanjay@gmail.com", "Selenium@1234");
 		
+		
+	}
+	
+	@AfterMethod
+	public void teardown()
+	{	
+		if(Page.driver != null) {
 		Page.quitBrowser();
-
+		}
 	}
 
 }
